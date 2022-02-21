b4eb529f8cdc37ee58f89ef0b470829b4921455b
Benchea Paul-Catalin <bencheapaulcatalin@gmail.com>
2022-02-21 16:40:00 +0200

    string array

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,5 +1,7 @@
 public class Lab1 {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        String[] array =  {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        System.out.println(array[2]);
     }
 }
