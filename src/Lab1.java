c8d8e73e8befeafde5dc3415349b439958e17e04
Benchea Paul-Catalin <bencheapaulcatalin@gmail.com>
2022-02-21 16:58:45 +0200

    calculus

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,8 +1,26 @@
 public class Lab1 {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        String[] array =  {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
-        System.out.println(array[2]);
+        String[] languages =  {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        System.out.println(languages[2]);
         int n = (int) (Math.random() * 1000000);
+        System.out.println(n);
+        n *= 3;
+        n += 0b10101;   //21
+        n += 0xFF;      //255
+        n *= 6;
+        System.out.println(n);
+        int sum = 0;
+        do {
+            if(n==0)
+                n = sum;
+            sum = 0;
+            while (n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+        }while (sum>10);
+        System.out.println( "Willy-nilly, this semester I will learn " + languages[sum]);
+
     }
 }
