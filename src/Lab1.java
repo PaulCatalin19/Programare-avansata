73909e08ea4fcaf6fb17d15a6d79bbfab4c29bae
Benchea Paul-Catalin <bencheapaulcatalin@gmail.com>
2022-02-21 16:41:35 +0200

    random number

--- src/Lab1.java
+++ src/Lab1.java
@@ -3,5 +3,6 @@ public class Lab1 {
         System.out.println("Hello World!");
         String[] array =  {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
         System.out.println(array[2]);
+        int n = (int) (Math.random() * 1000000);
     }
 }
